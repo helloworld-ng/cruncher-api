39e8abae2fc079bcd312c4fdf9933119eb0d704f
Opemipo Aikomo <opemipoaikomo@gmail.com>
2015-04-26 21:54:43 +0100

    Used production url

--- app/mailers/cruncher_mailer.rb
+++ app/mailers/cruncher_mailer.rb
@@ -1,6 +1,6 @@
 class CruncherMailer < ApplicationMailer
 	def crunched_statement(sheet, email)
-		base_url = "http://cruncher.dev/#/"
+		base_url = "http://cruncher.helloworld.ng/#/"
 		@sheet = sheet
 		@url = base_url + sheet.token + '/transactions'
       	@deleteUrl = base_url + sheet.token + '/delete'
