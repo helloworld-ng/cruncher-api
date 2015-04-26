c77d00977b6ffc2f6d9cdd8998e0eccb5ddb5b10
Opemipo Aikomo <opemipoaikomo@gmail.com>
2015-04-26 23:10:40 +0100

    Set up smtp/mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Sendgrid
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app36268553@heroku.com'],
+  :password       => ENV['g1a8zdfy'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
