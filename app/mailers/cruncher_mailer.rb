4a0d5c1f12ff4243f65f327f90345f58c9668fda
Lolu Bodunwa <btemilolu@gmail.com>
2015-12-26 15:57:35 +0100

    changed link in email to new domain

--- app/mailers/cruncher_mailer.rb
+++ app/mailers/cruncher_mailer.rb
@@ -1,11 +1,11 @@
 class CruncherMailer < ApplicationMailer
 	def crunched_statement(sheet, email)
-		base_url = "http://cruncher.helloworld.ng/#/"
+		base_url = "http://cruncherapp.co/#/"
 		@sheet = sheet
 		@url = base_url + sheet.token + '/transactions'
       	@deleteUrl = base_url + sheet.token + '/delete'
       	@email = email
-      	
+
 		mail(to: email, subject: 'Your statement has been crunched.')
 	end
 end
